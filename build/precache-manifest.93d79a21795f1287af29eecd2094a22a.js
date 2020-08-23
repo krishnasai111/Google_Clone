7762286bb8e5df1eb5eba15f58b2d9d6f08479d5
krishnasai111 <63897189+krishnasai111@users.noreply.github.com>
2020-08-23 23:23:40 +0530

    Add files via upload

---
+++ build/precache-manifest.93d79a21795f1287af29eecd2094a22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff36bd81b9e4801491dd60eed474df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bcd6ed684bb51957c18",
+    "url": "/static/css/main.609aa0a1.chunk.css"
+  },
+  {
+    "revision": "24b061cc6d1330137b1a",
+    "url": "/static/js/2.4cb2bf8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4cb2bf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bcd6ed684bb51957c18",
+    "url": "/static/js/main.f6a4e42e.chunk.js"
+  },
+  {
+    "revision": "8570b850cde403502982",
+    "url": "/static/js/runtime-main.43168123.js"
+  }
+]);
